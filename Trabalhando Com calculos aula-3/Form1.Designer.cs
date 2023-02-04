0c33464eb05a3bb8f0c3dc37a0fbb8726379998b
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-02-03 21:56:34 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalhando Com calculos aula-3/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Trabalhando_Com_calculos_aula_3
+{
+    partial class Calculater
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.textValorDigitado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResposta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.SystemColors.Control;
+            this.btn.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btn.Location = new System.Drawing.Point(133, 338);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(129, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "C A L C U L A R";
+            this.btn.UseVisualStyleBackColor = false;
+            // 
+            // textValorDigitado
+            // 
+            this.textValorDigitado.Location = new System.Drawing.Point(133, 134);
+            this.textValorDigitado.Name = "textValorDigitado";
+            this.textValorDigitado.Size = new System.Drawing.Size(176, 20);
+            this.textValorDigitado.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(71, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor:";
+            // 
+            // txtResposta
+            // 
+            this.txtResposta.Location = new System.Drawing.Point(200, 197);
+            this.txtResposta.Name = "txtResposta";
+            this.txtResposta.Size = new System.Drawing.Size(109, 20);
+            this.txtResposta.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(71, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Acrécimo de 10%:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(100, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C A L C U L A D O R A";
+            // 
+            // Calculater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(416, 426);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResposta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textValorDigitado);
+            this.Controls.Add(this.btn);
+            this.MaximizeBox = false;
+            this.Name = "Calculater";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox textValorDigitado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResposta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Trabalhando Com calculos aula-3/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalhando_Com_calculos_aula_3
+{
+    public partial class Calculater : Form
+    {
+        public Calculater()
+        {
+            InitializeComponent();
+        }
+    }
+}
